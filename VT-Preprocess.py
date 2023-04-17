26cd9a8703107b84bd94ce4b40fd1227d3315cff
StablumA <129173504+StablumA@users.noreply.github.com>
2023-04-17 19:26:03 -0300

    Add files via upload

--- VT-Preprocess.py
+++ VT-Preprocess.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
 import openpyxl
+import datetime
 #### importo los datos
 Ts_oct= pd.read_csv("10 Transacciones Octubre 2022.csv", sep= ';', parse_dates=["Fecha Hora"], dayfirst=True)
 Ts_oct= Ts_oct[Ts_oct["Fecha Hora"]>= pd.to_datetime('1-10-2022', format='%d-%m-%Y')]
 df=Ts_oct
 
-df["MES"]= df["Fecha Hora"].dt.month
-df= df[df["MES"]== 10]
+# renombro y corrijo
+df= df.rename(columns={"Fecha Hora":"FECHATRX"})
+df= df.rename(columns={"Hora":"HORA"})
+df= df.rename(columns={"Minutos":"MINUTO"})
+df= df.rename(columns={"Segundos":"SEGUNDO"})
+df["MES"]= df["FECHATRX"].dt.month
+df["AÑO"]= df["FECHATRX"].dt.year
+df["DIA"]=df["FECHATRX"].dt.day
+df['FECHATRX'] = df.apply(lambda row: datetime.datetime(row["AÑO"], row["MES"], row["DIA"], row['HORA'], row['MINUTO'], row['SEGUNDO']), axis=1)
+#df['FECHATRX'] = pd.to_datetime(df['DIA'].astype(str) + '/' + df['MES'].astype(str) + '/' + df['AÑO'].astype(str) + ' ' + df['HORA'].astype(str) + ':' + df['MINUTO'].astype(str) + ':' + df['SEGUNDO'].astype(str),format='%d/%m/%Y %H:%M:%S')
 
+
+df= df[df["MES"]== 10]
 ## filtro los duplicados
 print(df)
 print(len(df))
@@ -26,12 +37,8 @@ df['LINEA']=df.apply(lambda row: limp(row), axis=1)
 
 da = df
 # me llevo lo que no se repite
-df= df[["Interno","LINEA","Ramal","Tipo Trx","Fecha Hora","Hora","Tarifa Marcada","Tarifa Cobrada","Tarjeta","Legajo","Latitud","Longitud"]]
-df= df.rename(columns={"LINEA":"idlinea"})
-df= df.rename(columns={"Fecha Hora":"FECHATRX"})
-df= df.rename(columns={"Longitud":"longitude"})
-df= df.rename(columns={"Latitud":"latitude"})
-
-
+df= df.rename(columns={"Ramal":"idlinea","Tarjeta":"NROTARJETA","Longitud":"longitude","Latitud":"latitude","Tipo Trx":"CODIGOCONTRATO"})
+df= df[["NROTARJETA","FECHATRX","MES","latitude","longitude","idlinea","CODIGOCONTRATO"]]
 da.to_csv("VT_OCT.csv")
-df.to_csv("10-TX-SData.csv")
+df.to_csv("./Outputs/10-TX-SData.csv")
+print('GENERADO VT_OCT.csv EN CARPETA OUTPUTS')
